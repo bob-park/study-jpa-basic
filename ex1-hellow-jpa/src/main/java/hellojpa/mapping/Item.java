4c1697708675c9527f0ae38c5aee165aa4d70fc6
phw11 <hwpark11@kakao.com>
2021-08-15 15:03:41 +0900

    feat : 구현 클래스마다 테이블 전략

--- ex1-hellow-jpa/src/main/java/hellojpa/mapping/Item.java
+++ ex1-hellow-jpa/src/main/java/hellojpa/mapping/Item.java
@@ -9,11 +9,12 @@ import javax.persistence.*;
  *  !JPA 의 상속 맵핑의 default 가 single table 전략이다.
  * </pre>
  *
- * <p>@{@link DiscriminatorColumn} 이 있을 필욘 없지만, 유지보수 측면에서 명확히 하려면 필요하다.
+ * <p>@{@link DiscriminatorColumn} 이 있을 필욘 없지만, 유지보수 측면에서 선언하는 것이 좋다.
  */
 @Entity
 // @Inheritance(strategy = InheritanceType.JOINED) // 조인 전략
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일 테이블 전략
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일 테이블 전략
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 구현 클래스마다 테이블 전략
 @DiscriminatorColumn // 부모 테이블에 type 전략 추가 - 단, Single Table 전략인 경우 무조건 @DiscriminatorColumn 적용된다.
 public class Item {
 
