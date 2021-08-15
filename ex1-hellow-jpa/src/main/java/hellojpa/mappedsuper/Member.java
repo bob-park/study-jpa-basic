c7510399d76a76ed3115c9068f780e5ca35f095e
phw11 <hwpark11@kakao.com>
2021-08-15 15:27:55 +0900

    feat : MappedSuperClass 추가

---
+++ ex1-hellow-jpa/src/main/java/hellojpa/mappedsuper/Member.java
@@ -0,0 +1,29 @@
+package hellojpa.mappedsuper;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Member extends BaseEntity {
+
+  @Id @GeneratedValue private Long id;
+
+  private String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
