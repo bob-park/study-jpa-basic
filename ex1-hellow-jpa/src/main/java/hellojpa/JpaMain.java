6a7b73af8b163d8dc82b5fbf26877f17a9abd3ab
phw11 <hwpark11@kakao.com>
2021-08-16 15:13:08 +0900

    feat : 예제 코드 추가

--- ex1-hellow-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hellow-jpa/src/main/java/hellojpa/JpaMain.java
@@ -38,9 +38,17 @@ public class JpaMain {
       em.flush();
       em.clear();
 
-      Member findMember = em.find(Member.class, member.getId());
+      // * JPA 에서 member 를 가져오는 쿼리에 team 까지 같이 가져오는 쿼리(Join Query)를 실행해버린다.
+      //      Member findMember = em.find(Member.class, member.getId());
 
-      printMemberAndTeam(findMember);
+      // * getRefernece() 만 실행하면, DB Query 를 실행하지 않은다. - 실제 member 가 사용되는 시점에 쿼리가 실행된다.
+      Member findMember = em.getReference(Member.class, member.getId());
+
+      // ! member 와 Team 을 출력할 경우
+      //      printMemberAndTeam(findMember);
+
+      // ! member 만 출력할 경우
+      printMember(findMember);
 
       tx.commit();
     } catch (Exception e) {
@@ -52,6 +60,21 @@ public class JpaMain {
     }
 
     emf.close();
+  }
+
+  private static void printMember(Member findMember) {
+
+    // ! JPA Proxy 에서 생성한 가짜 Entity - DB Query 실행 후 진짜 entity 생성하여 method() 를 실행하게 된다.
+    // getName() -> Proxy -> Persistence Context 초기화 요청 -> DB Query 실행 -> 실제 Entity 생성 ->
+    // Entity.getName() 실행
+    System.out.println("findMember = " + findMember.getClass());
+    System.out.println(
+        "findMember.id = " + findMember.getId()); // JPA 조회 시 ID 로 조회하기 때문에 ID 는 들어간다.
+
+    // ! getReference() 사용시 이때 DB Query 가 실행된다.
+    // getName() -> Proxy -> Persistence Context 초기화 요청 -> DB Query 실행 -> 실제 Entity 생성 ->
+    // Entity.getName() 실행
+    System.out.println("name = " + findMember.getName());
   }
 
   private static void printMemberAndTeam(Member findMember) {
