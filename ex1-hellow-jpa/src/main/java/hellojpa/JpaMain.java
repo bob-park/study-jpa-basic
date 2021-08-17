f248045052c61f67013b4713def854e1f2a99144
phw11 <hwpark11@kakao.com>
2021-08-17 14:54:42 +0900

    feat : Embedded type 예제 코드 추가

--- ex1-hellow-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hellow-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -23,6 +24,13 @@ public class JpaMain {
 
     // code
     try {
+
+      Member member = new Member();
+      member.setName("memberA");
+      member.setAddress(new Address("city", "street", "zipcode"));
+      member.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
+
+      em.persist(member);
 
       tx.commit();
     } catch (Exception e) {
